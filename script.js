f4ece9e92427e4612fdf02cc3c90e7942dc9a847
AleSolis24 <aletitan24@icloud.com>
2024-01-28 17:40:42 -0500

    added search funciton for the user and working on getting all weather for the cities using the fetch method for the api

--- script.js
+++ script.js
@@ -5,24 +5,32 @@ let searchHistory = document.getElementById('searchHistory');
 let todayWeather = document.getElementById('todayWeather');
 let forcast = document.getElementById('forecast');
 const cityInput = document.getElementById('cityInput')
+const searchBtn = document.getElementById('searchBtn');
 
-searchForm.addEventListener('submit', function (event) {
-    event.defaultPrevented();
 
-    const cities = cityInput.ariaValueMax.trim();
 
-    if (cities) {
-        cityWeather(cities);
-        cityInput.value = ''; 
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const cities = cityInput.value.trim();
+
+    if(!cities) {
+        getAllWeather(cities);
+        cityInput.innerHTML = '';
+    }
+});
+
+// event listner for the user to hit the searchbtn to search for there selected city 
+searchBtn.addEventListener('click', function() {
+    const certainCities = searchHistory.querySelector('.selected');
+    if(certainCities) {
+        const cities = selectedCities.textContent;
+        getAllWeather(cities);
     }
 });
 
 
-searchHistory.addEventListener('click', function (event) {
-    if (event.target.tagName == 'li') {
-        const cities = event.target.textContent;
-        cityWeather(cities); 
-    }
+// function to get all weather using the fetch method to get the api 
+async function getAllWeather(cities) {
 
-});
-
+}
